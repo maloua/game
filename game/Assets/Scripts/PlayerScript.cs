39d5fc0904e4750880b3bf0cd0e4f15897157eff
Malou <malou_allertz@hotmail.com>
2018-05-04 11:25:05 +0200

    add script to move player forward continuously

---
+++ game/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		rb.position += new Vector3 (0, 0, speed);
+	}
+}
