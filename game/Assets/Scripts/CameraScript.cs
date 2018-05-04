8a2c80adf0af14eb69c7fe96f46a71aa1c417834
Malou <malou_allertz@hotmail.com>
2018-05-04 11:36:52 +0200

    add script to move main camera with player

---
+++ game/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	public GameObject player;
+	public float x;
+	public float y;
+	public float z;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = player.transform.position - new Vector3 (x, y, z);
+		transform.position = offset;
+	}
+	
+	void Update () {
+		transform.position = player.transform.position - offset;
+	}
+}
